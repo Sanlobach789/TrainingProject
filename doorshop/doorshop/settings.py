ea28ffdc2db2fd83b642637668592db9cc86444c
Aleksandr Lobach <Sanlobach789@gmail.com>
2021-02-15 19:19:04 +0300

    Update settings.py

--- doorshop/doorshop/settings.py
+++ doorshop/doorshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(k+mvi$uj(3=!fe^@)4!(l(-4-ip3i#h147gzsk!@s13vlb6jk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
