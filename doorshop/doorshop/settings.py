a1e08cd75d8e840a5beb5174b0c1e06bfc60749c
Lobach Aleksandr <sanlobach789@gmail.com>
2021-02-16 12:41:32 +0300

    created project structure

--- doorshop/doorshop/settings.py
+++ doorshop/doorshop/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(k+mvi$uj(3=!fe^@)4!(l(-4-ip3i#h147gzsk!@s13vlb6jk'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
